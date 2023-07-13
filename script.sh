b6d5f05c91268b182fb34609e81abf50fff82194
alexeykosh <alexeykochevoy@gmail.com>
2023-07-13 17:36:27 +0200

    preparing for the cluster

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=slovotvirModel      ## Name of the job
+#SBATCH --output=slovotvirOut.out    ## Output file
+#SBATCH --ntasks-per-node=10
+## notifications par mail
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=alexey.koshevoy@univ-amu.fr
+
+## Load the python interpreter
+module load python
+
+## Execute the python script and pass the argument/input '90'
+srun python src/model.py
