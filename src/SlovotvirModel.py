c2fc798d2da79355832e112980a5a43e58d7db56
alexeykosh <alexeykochevoy@gmail.com>
2024-12-20 15:35:15 +0100

    model corrections

--- src/SlovotvirModel.py
+++ src/SlovotvirModel.py
@@ -1,163 +1,137 @@
-from re import T
 import numpy as np
+from multiprocessing import Pool
 import pickle
 
-# unpickle n_words, translation_len, votes, n_translations from data folder
-with open('../data/n_words.pkl', 'rb') as f:
+# Load data
+with open('data/n_words.pkl', 'rb') as f:
     n_words = pickle.load(f)
 
-with open('../data/translation_len.pkl', 'rb') as f:
+with open('data/translation_len.pkl', 'rb') as f:
     translation_len = pickle.load(f)
 
-with open('../data/votes.pkl', 'rb') as f:
+with open('data/votes.pkl', 'rb') as f:
     votes = pickle.load(f)
     votes = np.array([sum(v) for v in votes.values()])
 
-with open('../data/n_translations.pkl', 'rb') as f:
+with open('data/n_translations.pkl', 'rb') as f:
     n_translations = pickle.load(f)
 
-with open('../data/true_likes.pkl', 'rb') as f:
+with open('data/true_likes.pkl', 'rb') as f:
     true_likes = pickle.load(f)
 
-
-# def shannon_diversity_index(likes):
-#     '''Calculates the Shannon diversity index.'''
-#     likes_sorted = np.sort(likes)[::-1]
-#     _, like_counts = np.unique(likes_sorted, return_counts=True)
-#     like_proportions = like_counts / np.sum(like_counts)
-#     return -np.sum(like_proportions * np.log(like_proportions))
-
-
-# def simpson_diversity_index(likes):
-#     '''Calculates the Simpson diversity index.'''
-#     likes_sorted = np.sort(likes)[::-1]
-#     _, like_counts = np.unique(likes_sorted, return_counts=True)
-#     like_proportions = like_counts / np.sum(like_counts)
-#     return np.sum(like_proportions ** 2)
-
-
-# def summary_func(likes):
-#     '''Calculates the summary statistics of a list of likes.'''
-#     return np.mean(likes == 1), \
-#         likes.max() / np.sum(likes), \
-#             shannon_diversity_index(likes), \
-#                 simpson_diversity_index(likes), \
-#                     np.mean(likes), \
-#                         np.median(likes)
-
-
-def min_max_scaling_numpy(data, new_min=1, new_max=10):
-    '''Scales data to a new range.'''
-    if data.size == 0:
-        return data
-    original_min = np.min(data)
-    original_max = np.max(data)
-
-    if original_min == original_max or new_min == new_max:
-        return data
-    scaled_data = ((data - original_min) / (original_max - original_min)) * (new_max - new_min) + new_min
-    return scaled_data
-
-
-def adjust_entropy(prob_distribution, temperature):
-    '''Adjusts entropy of a probability distribution.'''
-    temperature = max(1e-3, temperature)
-    scaled_probs = np.power(prob_distribution, 1 / temperature)
-    adjusted_distribution = scaled_probs / np.sum(scaled_probs)
-
-    return adjusted_distribution
+# usefull functions
+def safelog(x):
+    '''Safe log function.'''
+    with np.errstate(divide='ignore'):
+        return np.log(x)
 
 
 class SlovotvirModel:
     '''Slovotvir model.'''
-
-    def __init__(self, n_words, 
-                 translation_len, 
-                 votes, 
-                 n_translations, 
-                 a, b, t) -> None:
-        # epoch states 
-        self.n_words = n_words
+    def __init__(self, n_words, translation_len, votes, n_translations, a, b, t):
+        self.cum_words = np.cumsum(n_words)
         self.translation_len = translation_len
         self.votes = votes
         self.n_translations = n_translations
+        self.a = a # length bias
+        self.b = b # frequency bias (translation selection)
+        self.t = t # frequency bias (word selection)
+        # Initialize words as a dictionary of numpy arrays for likes and lengths
+        self.words = {i: [np.array([], dtype=int), np.array([], dtype=int)] 
+                      for i in range(self.cum_words[-1])}
 
-        # cumulative words
-        self.cum_words = np.cumsum(n_words)
-
-        # hyperparameters
-        self.a = a
-        self.b = b
-        self.t = t
-
-        self.words = dict()
-        for i in range(sum(n_words)):
-            self.words[i] = [[], []] # [[likes], [lengths]]
-
-    
-    def like_prob(self, 
-                  lengths, 
-                  likes) -> np.ndarray:
+    def like_prob(self, lengths, likes):
         '''Calculates the probability of liking each translation.'''
-        probs = lengths ** self.a + likes ** self.b
+        probs = np.exp(self.a * safelog(lengths) + self.b * safelog(likes))
         return probs / probs.sum()
-    
-    def run_epoch(self, epoch) -> None:
-        '''Runs an epoch.'''
-        # n_words = self.n_words[epoch]
+
+    def run_epoch(self, epoch):
+        '''Runs a single epoch of the model.'''
         n_translations = self.n_translations[epoch]
         votes = self.votes[epoch]
         cum_words = self.cum_words[epoch]
 
-        # distribute n_translation into an array of length cum_words
-        translation_num = np.random.multinomial(n_translations, np.ones(cum_words)/cum_words)
+        # Distribution of translations across words
+        translation_num = np.random.multinomial(n_translations, np.ones(cum_words) / cum_words)
 
-        # get the indices of non-zero elements in translation_num
-        non_zero_indices = np.nonzero(translation_num)[0]
-
-        # update the words list only for non-zero indices
-        for i in non_zero_indices:
+        # Update the words with new translations
+        indices = np.nonzero(translation_num)[0]
+        for i in indices:
             n = translation_num[i]
-            self.words[i][0] += [0] * n # initialize likes to 0
-            self.words[i][1] += np.random.choice(self.translation_len, n).tolist()
+            new_lengths = np.random.choice(self.translation_len, size=n)
+            new_likes = np.zeros(n, dtype=int)
+            self.words[i][0] = np.concatenate([self.words[i][0], new_likes])
+            self.words[i][1] = np.concatenate([self.words[i][1], new_lengths])
 
+        # Voting mechanism
         indices = np.array([i for i in range(cum_words) if len(self.words[i][0]) > 0])
-        cum_likes = np.array([sum(self.words[i][1]) for i in indices])
-        indices = np.random.choice(indices,
-                                   size=votes,
-                                   p=adjust_entropy(cum_likes / cum_likes.sum(), temperature=self.t),
-                                   replace=True)
-        
-        for _ in indices:
-            # get the word
-            likes = min_max_scaling_numpy(np.array(self.words[_][0]))
-            lengths = min_max_scaling_numpy(1 / np.array(self.words[_][1]))
+        if len(indices) > 0:
+            cum_likes = np.array([np.max(self.words[i][0]) + 1 for i in indices])
+            cum_likes = np.exp(self.t * safelog(cum_likes))
+            cum_likes = cum_likes / cum_likes.sum()
+            chosen_indices = np.random.choice(indices,
+                                              size=votes,
+                                              p=cum_likes,
+                                              replace=True)
+            for idx in chosen_indices:
+                lengths = self.words[idx][1]
+                # laplace smoothing of likes:
+                likes = self.words[idx][0] + 1
+                probs = self.like_prob(lengths, likes)
+                choice = np.random.choice(len(probs),
+                                          p=probs)
+                self.words[idx][0][choice] += 1 # adding a like
 
-            # get the probability of liking each translation
-            probs = self.like_prob(lengths, likes)
-
-            # choose a translation
-            translation = np.random.choice(likes, p=probs)
-
-            # update the number of likes for the translation
-            self.words[_][0][likes.tolist().index(translation)] += 1
-
-    def run(self) -> None:
-        '''Runs the model.'''
-        n_epochs = len(self.n_words)
+    def run(self, n_epochs):
+        '''Runs the model for n_epochs.'''
         for epoch in range(n_epochs):
             self.run_epoch(epoch)
 
-def run_model(params):
-    '''Runs the model with given hyperparameters.'''
-    a, b, t = params
-    model = SlovotvirModel(n_words, translation_len, votes, n_translations, a, b, t)
-    model.run()
-    # combine all likes list from the model
-    likes = []
-    for i in range(len(model.words)):
-        likes += model.words[i][0]
+def run_model_instance(args):
+    '''Run a single model instance.'''
+    model, n_epochs = args
+    model.run(n_epochs)
+    return model
 
-    likes = np.array(likes)
+def refactor(model):
+    '''Refactor the model results.'''
+    # Extract values from the model
+    word_values = list(model.words.values())
 
-    return np.histogram(likes, bins=1000)[0]
+    # Concatenate likes
+    likes = np.concatenate([vals[0] for vals in word_values])
+
+    # Concatenate lengths
+    lengths = np.concatenate([vals[1] for vals in word_values])
+
+    # Sort and return results
+    sorted_likes = likes[np.argsort(likes)[::-1]]
+    sorted_lengths = lengths[np.argsort(likes)[::-1]]
+
+    return sorted_likes, sorted_likes * sorted_lengths
+
+def run_model_parallel(a, b, t, num_runs, batch_size=1000):
+    '''Run the model in parallel for multiple instances.'''
+    num_processes = 32  # Number of processes
+
+    # Calculate number of batches
+    num_batches = (num_runs + batch_size - 1) // batch_size
+
+    results = []
+
+    for batch_idx in range(num_batches):
+        start_idx = batch_idx * batch_size
+        end_idx = min((batch_idx + 1) * batch_size, num_runs)
+  
+        # Create model instances for the current batch
+        models = [SlovotvirModel(n_words, translation_len, votes, n_translations, 
+                                 a[i], b[i], t[i]) for i in range(start_idx, end_idx)]
+
+        # Use multiprocessing to run models for the current batch
+        with Pool(num_processes) as pool:
+            batch_results = pool.map(run_model_instance, 
+                                     [(model, len(n_words)) for model in models])
+
+        results.extend([refactor(model) for model in batch_results])
+        
+    return results
--- src/helpers.py
+++ src/helpers.py
@@ -1,18 +1,22 @@
+'''Helper functions for the Bayesian inference of the Slovotvir model.'''
+
 import arviz as az
 from tabulate import tabulate
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib import lines
-
 from bayesflow.amortizers import AmortizedPosterior
 from bayesflow.networks import (InvertibleNetwork, 
-                                DeepSet)
+                                DeepSet,
+                                SetTransformer)
 from bayesflow.trainers import (Trainer, 
                                 SimulationDataset)
 import tensorflow as tf
 
-def generate_latex_table(ps, post_samples):
+
+def generate_latex_table(ps,
+                         post_samples):
     '''
     Generate a latex table from the posterior samples of the parameters.
     '''
@@ -27,7 +31,10 @@ def generate_latex_table(ps, post_samples):
 
     return latex_table
 
-def configure_input(forward_dict, prior_means, prior_stds):
+
+def configure_input(forward_dict, 
+                    prior_means, 
+                    prior_stds):
     """
     Function to configure the simulated quantities (i.e., simulator outputs)
     into a neural network-friendly (BayesFlow) format.
@@ -55,6 +62,7 @@ def configure_input(forward_dict, prior_means, prior_stds):
 
     return out_dict
 
+
 def binning(likes):
     '''
     Binning of the likes distribution on a log scale.
@@ -74,7 +82,10 @@ def binning(likes):
     return bin_sums
 
 
-def plot_posterior(post_samples_, param_names, true_values, save):
+def plot_posterior(post_samples_, 
+                   param_names, 
+                   true_values, 
+                   save):
     '''
     Plot the posterior distribution of the parameters. 
 
@@ -100,17 +111,31 @@ def plot_posterior(post_samples_, param_names, true_values, save):
         sns.kdeplot(post_samples_[:, i], ax=axs[i], color='blue')
 
         # Get KDE curve for each subplot
-        x_values, y_values = sns.kdeplot(post_samples_[:, i], ax=axs[i], color='blue').lines[0].get_data()
+        x_values, y_values = sns.kdeplot(post_samples_[:, i], 
+                                         ax=axs[i], 
+                                         color='blue').lines[0].get_data()
+
+        # Normalize posterior density
+        y_values /= np.trapz(y_values, x_values)
 
         # Fill between specified points for KDE plot
         post_hdi = az.hdi(post_samples_[:, i], hdi_prob=0.95)
-        axs[i].fill_between(x_values, y_values, where=(x_values >= post_hdi[0]) & (x_values <= post_hdi[1]), color='blue', alpha=0.3)
+        axs[i].fill_between(x_values, y_values, where=(x_values >= post_hdi[0]) & 
+                            (x_values <= post_hdi[1]), color='blue', alpha=0.3)
 
         # Plot prior density (assuming uniform for first two parameters and lognormal for the third)
-        if i < 2:
-            sns.kdeplot(np.random.uniform(0, 10, 10000), ax=axs[i], color='black', linestyle='--')
+        if i == 0:
+            prior_samples = np.random.uniform(-5, 1, 10000)
+        elif i == 1:
+            prior_samples = np.random.uniform(-2, 2, 10000)
         else:
-            sns.kdeplot(np.random.lognormal(0, sigma=0.5, size=10000), ax=axs[i], color='black', linestyle='--')
+            prior_samples = np.random.lognormal(0, sigma=0.5, size=10000)
+
+        sns.kdeplot(prior_samples, ax=axs[i], color='black', linestyle='--')
+
+        # Normalize prior density
+        prior_x_values, prior_y_values = sns.kdeplot(prior_samples, ax=axs[i], color='black', linestyle='--').lines[0].get_data()
+        prior_y_values /= np.trapz(prior_y_values, prior_x_values)
 
         # Plot true values
         axs[i].axvline(true_values[i], color='red', linestyle='--')
@@ -142,16 +167,29 @@ def plot_posterior(post_samples_, param_names, true_values, save):
     plt.show()
 
 
-def train_and_amortize(train_data, batch_size, test_data, epochs, num_params, summary_dim, learning_rate, prior_means, prior_stds):
+def train_and_amortize(train_data, 
+                       batch_size, 
+                       test_data, 
+                       epochs, 
+                       num_params, 
+                       summary_dim, 
+                       learning_rate, 
+                       prior_means, 
+                       prior_stds):
     '''
     Train and amortize given the simulation data using the BayesFlow framework.
     '''
     summary_net = DeepSet(summary_dim=summary_dim)
+    # summary_net = SetTransformer(input_dim=train_data["sim_data"].shape[1],)
     inference_net = InvertibleNetwork(num_params=num_params, num_coupling_layers=4)
     amortizer = AmortizedPosterior(inference_net, summary_net, name="slovotvir_amortizer")
     
     # Define trainer
-    trainer = Trainer(amortizer=amortizer, configurator=lambda x: configure_input(x, prior_means, prior_stds), memory=True)
+    trainer = Trainer(amortizer=amortizer, 
+                      configurator=lambda x: configure_input(x, 
+                                                             prior_means, 
+                                                             prior_stds), 
+                      memory=True)
     
     # Define learning rate schedule
     schedule = tf.keras.optimizers.schedules.CosineDecay(learning_rate, 
@@ -163,10 +201,110 @@ def train_and_amortize(train_data, batch_size, test_data, epochs, num_params, su
     # Run training
     history = trainer.train_offline(simulations_dict=train_data, epochs=epochs, 
                                     batch_size=batch_size, optimizer=optimizer, 
-                                    validation_sims=test_data,
-                                    early_stopping=True, patience=20)
+                                    validation_sims=test_data)
     
     # Save the trainer
     trainer._save_trainer("model")
     
     return history, trainer, amortizer
+
+
+def letter_subplots(axes=None, letters=None, xoffset=-0.1, yoffset=1.0, **kwargs):
+    """Add letters to the corners of subplots (panels). By default each axis is
+    given an uppercase bold letter label placed in the upper-left corner.
+    Args
+        axes : list of pyplot ax objects. default plt.gcf().axes.
+        letters : list of strings to use as labels, default ["A", "B", "C", ...]
+        xoffset, yoffset : positions of each label relative to plot frame
+          (default -0.1,1.0 = upper left margin). Can also be a list of
+          offsets, in which case it should be the same length as the number of
+          axes.
+        Other keyword arguments will be passed to annotate() when panel letters
+        are added.
+    Returns:
+        list of strings for each label added to the axes
+    Examples:
+        Defaults:
+            >>> fig, axes = plt.subplots(1,3)
+            >>> letter_subplots() # boldfaced A, B, C
+        
+        Common labeling schemes inferred from the first letter:
+            >>> fig, axes = plt.subplots(1,4)        
+            >>> letter_subplots(letters='(a)') # panels labeled (a), (b), (c), (d)
+        Fully custom lettering:
+            >>> fig, axes = plt.subplots(2,1)
+            >>> letter_subplots(axes, letters=['(a.1)', '(b.2)'], fontweight='normal')
+        Per-axis offsets:
+            >>> fig, axes = plt.subplots(1,2)
+            >>> letter_subplots(axes, xoffset=[-0.1, -0.15])
+            
+        Matrix of axes:
+            >>> fig, axes = plt.subplots(2,2, sharex=True, sharey=True)
+            >>> letter_subplots(fig.axes) # fig.axes is a list when axes is a 2x2 matrix
+
+    See also: https://github.com/matplotlib/matplotlib/issues/20182
+    """
+
+    # get axes:
+    if axes is None:
+        axes = plt.gcf().axes
+    # handle single axes:
+    try:
+        iter(axes)
+    except TypeError:
+        axes = [axes]
+
+    # set up letter defaults (and corresponding fontweight):
+    fontweight = "bold"
+    ulets = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'[:len(axes)])
+    llets = list('abcdefghijklmnopqrstuvwxyz'[:len(axes)])
+    if letters is None or letters == "A":
+        letters = ulets
+    elif letters == "(a)":
+        letters = [ "({})".format(lett) for lett in llets ]
+        fontweight = "normal"
+    elif letters == "(A)":
+        letters = [ "({})".format(lett) for lett in ulets ]
+        fontweight = "normal"
+    elif letters in ("lower", "lowercase", "a"):
+        letters = llets
+
+    # make sure there are x and y offsets for each ax in axes:
+    if isinstance(xoffset, (int, float)):
+        xoffset = [xoffset]*len(axes)
+    else:
+        assert len(xoffset) == len(axes)
+    if isinstance(yoffset, (int, float)):
+        yoffset = [yoffset]*len(axes)
+    else:
+        assert len(yoffset) == len(axes)
+
+    # defaults for annotate (kwargs is second so it can overwrite these defaults):
+    my_defaults = dict(fontweight=fontweight, fontsize='large', ha="center",
+                       va='center', xycoords='axes fraction', annotation_clip=False)
+    kwargs = dict( list(my_defaults.items()) + list(kwargs.items()))
+
+    list_txts = []
+    for ax,lbl,xoff,yoff in zip(axes,letters,xoffset,yoffset):
+        t = ax.annotate(lbl, xy=(xoff,yoff), **kwargs)
+        list_txts.append(t)
+    return list_txts
+
+
+def gini(array):
+    """Calculate the Gini coefficient of a numpy array."""
+    array = array.flatten()
+    if np.amin(array) < 0:
+        # Values cannot be negative:
+        array -= np.amin(array)
+    array = np.sort(array)
+    index = np.arange(1,array.shape[0]+1)
+    n = array.shape[0]
+    # Gini coefficient:
+    return ((np.sum((2 * index - n  - 1) * array)) / (n * np.sum(array)))
+
+def preprocess_input(input):
+    likes = np.array([binning(i[0]) for i in input])
+    lengths = np.array([binning(i[1]) for i in input])
+
+    return np.concatenate((likes, lengths), axis=1)
